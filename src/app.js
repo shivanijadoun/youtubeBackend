db15a23b31caec9343abc2f45ec8abf89beb19be
Shivani <shivanirana0080@gmail.com>
2024-08-26 12:57:36 +0530

    register

--- src/app.js
+++ src/app.js
@@ -10,10 +10,22 @@ app.use(cors({
 }))
 
 app.use(express.json({limit:"16kb"}))
-AbortController.use(express.urlencoded({extended:true,limit:"16kb"}))
+app.use(express.urlencoded({extended:true,limit:"16kb"}))
 
 app.use(express.static("public"))
 app.use(cookieParser())
+
+
+//routes import
+
+import userRouter from './routes/user.router.js' 
+
+
+//routes declaration
+app.use("/api/v1/users",userRouter)
+
+//http://localhost:800/api/v1/users/register
+
 
 
 export { app }
--- src/index.js
+++ src/index.js
@@ -1,28 +1,25 @@
-//require('dotenv').config({path:./env})
+// require('dotenv').config({path: './env'})
 import dotenv from "dotenv"
-// import mongoose from "mongoose";
-// import {DB_NAME} from "./constants"
-
-
 import connectDB from "./db/index.js";
-
+import {app} from './app.js'
 dotenv.config({
-    path: './env'
+    path: './.env'
 })
+
 
 
 connectDB()
-.then(()=>{
-
-    //write app handle error in this assignment from app.on
-    app.listen(process.env.PORT || 8000 ,()=>{
-      console.log(`Server is running at port : ${process.env.PORT}`);
+.then(() => {
+    app.listen(process.env.PORT || 5000, () => {
+        console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
     })
 })
-.catch(()=>{
-    console.log(`MONGO db connection failed !!!`,error);
-
+.catch((err) => {
+    console.log("MONGO db connection failed !!! ", err);
 })
+
+
+
 
 
 
@@ -33,41 +30,22 @@ connectDB()
 /*
 import express from "express"
 const app = express()
+( async () => {
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        app.on("errror", (error) => {
+            console.log("ERRR: ", error);
+            throw error
+        })
 
-(async()=>{
-  try {
-   await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-    app.on("error",(error)=>{
-        console.log("ERROR:",error);
-        throw error
-    })
+        app.listen(process.env.PORT, () => {
+            console.log(`App is listening on port ${process.env.PORT}`);
+        })
 
-    app.listen(process.env.PORT,()=>{
-        console.log(`App is listening on port ${process.env.PORT}`);
-
-    })
-  } catch (error) {
-    console.error("ERROR:",error)
-    throw err
-  }
+    } catch (error) {
+        console.error("ERROR: ", error)
+        throw err
+    }
 })()
 
 */
-
-
-
-
-
-
-
-
-// function connectDB(){}
-
-// connectDB();
-
-
-//when data is coming from url then we use req.params
-
-//we can take data from cookies as well from data.cookies
-
-//we use middlewares with the help  use
